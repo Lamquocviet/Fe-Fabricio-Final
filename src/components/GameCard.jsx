c316717a6d21d275ddcdebf5e18c30db69572407
Lamquocviet <lamquocviet2005.nvt@gmail.com>
2026-04-30 23:26:18 +0700

    gamecard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,21 +2,19 @@ import React, { use } from "react";
 import { useNavigate } from "react-router";
 
 export default function GameCard({ game }) {
-
-  
   const navigate = useNavigate();
   const handleClickView = () => {
     navigate(`/games/${game.id}`);
-  }
+  };
   return (
     <article className="flex h-full flex-col overflow-hidden rounded-[22px] border border-white/10 bg-[#141517] shadow-[0_8px_24px_rgba(0,0,0,0.24)] transition hover:-translate-y-0.5 hover:border-white/15">
       {/* Image */}
       <div className="relative overflow-hidden">
         <img
-  src={game.image || game.thumbnailUrl}
-  alt={game.title}
-  className="h-44 w-full object-cover"
-/>
+          src={game.image || game.thumbnailUrl}
+          alt={game.title}
+          className="h-44 w-full object-cover"
+        />
 
         <button className="absolute right-4 top-4 flex h-9 w-9 items-center justify-center rounded-[14px] border border-red-500/25 bg-red-500/10 text-sm text-white">
           ♡
@@ -30,9 +28,7 @@ export default function GameCard({ game }) {
           <h3 className="truncate text-lg font-bold text-white">
             {game.title}
             {game.type && (
-              <span className="ml-2 text-sm text-white">
-                ({game.type})
-              </span>
+              <span className="ml-2 text-sm text-white">({game.type})</span>
             )}
           </h3>
 
@@ -56,7 +52,8 @@ export default function GameCard({ game }) {
           <span
             className={`text-2xl font-bold ${
               game.price === "Free" ? "text-[#1ee59b]" : "text-[#ffb14a]"
-            }`}>
+            }`}
+          >
             {game.price}
           </span>
 
@@ -66,7 +63,10 @@ export default function GameCard({ game }) {
         </div>
 
         <div className="mt-auto grid grid-cols-2 gap-2 pt-2">
-          <button onClick={handleClickView} className="rounded-xl bg-linear-to-r from-[#ff6a4a] to-[#ff4d61] px-4 py-2 text-sm font-semibold text-white cursor-pointer">
+          <button
+            onClick={handleClickView}
+            className="rounded-xl bg-linear-to-r from-[#ff6a4a] to-[#ff4d61] px-4 py-2 text-sm font-semibold text-white cursor-pointer"
+          >
             View
           </button>
 
